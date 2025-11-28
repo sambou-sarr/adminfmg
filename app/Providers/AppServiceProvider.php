75a78e84cdaf733547627c898f0c7b611b02d06f
sambou <sarrsambou03@gmail.com>
2025-11-28 10:57:44 +0000

    first commit1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Observers\TacheObserver;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
          Schema::defaultStringLength(91);
          Tache::observe(TacheObserver::class);
+         URL::forceScheme('https');
             // Gate pour le panel admin
         Gate::define('access-admin', function ($user) {
             return $user->hasRole('super_admin');
